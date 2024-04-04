69fcaecc84a6cbfe867a599fc454a11ef1a30a57
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2024-04-04 21:20:41 +0300

    add Profile styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,23 +1,25 @@
+import './Profile.css';
+
 const Profile = ({ user }) => {
   return (
     <div className="profile">
       <div className="description">
         <img src={user.avatar} alt="User avatar" className="avatar" />
         <p className="name">{user.username}</p>
-        <p className="tag">{user.tag}</p>
+        <p className="tag">@{user.tag}</p>
         <p className="location">{user.location}</p>
       </div>
 
       <ul className="stats">
-        <li>
+        <li className="stats-item">
           <span className="label">Followers</span>
           <span className="quantity">{user.stats.followers}</span>
         </li>
-        <li>
+        <li className="stats-item">
           <span className="label">Views</span>
           <span className="quantity">{user.stats.views}</span>
         </li>
-        <li>
+        <li className="stats-item">
           <span className="label">Likes</span>
           <span className="quantity">{user.stats.likes}</span>
         </li>
